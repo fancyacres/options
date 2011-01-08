b9903b7a15118663624b295aa4bbb633e4caf969
David Sidlinger <david.sidlinger@gmail.com>
2011-01-08 17:09:48 -0600

    A working NuGet package

--- BuildPackage.bat
+++ BuildPackage.bat
@@ -1,3 +1,3 @@
 mkdir nuget-package
-MSBuild .\src\Options.sln "/T:Clean;Build" /P:Configuration=Release
-.\tools\nuget.exe p .\src\Options.nuspec -b .\src\Options\bin\Release -o .\nuget-package
+MSBuild .\src\Options.sln "/T:Clean;Build" "/P:Configuration=Release;OutputPath=.\bin\Package\lib"
+.\tools\nuget.exe p .\src\Options.nuspec -b .\src\Options\bin\Package -o .\nuget-package
