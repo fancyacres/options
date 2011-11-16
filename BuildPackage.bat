49d6c4efaaf4cdb531779bc7b934ff18ea402b4d
David Sidlinger <david.sidlinger@gmail.com>
2011-11-16 16:41:09 -0600

    Better NuGetting

--- BuildPackage.bat
+++
@@ -1,3 +0,0 @@
-mkdir nuget-package
-MSBuild .\src\Options.sln "/T:Clean;Build" "/P:Configuration=Release;OutputPath=.\bin\Package\lib"
-.\tools\nuget.exe p .\src\Options.nuspec -b .\src\Options\bin\Package -o .\nuget-package
--- src/Options/Properties/AssemblyInfo.cs
+++ src/Options/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Options")]
-[assembly: AssemblyDescription("A handy alternative to null")]
+[assembly: AssemblyDescription("Options replicates the option type from functional programming with an API more suited to C# and VB.NET.")]
 [assembly: Guid("1f26ea13-0023-4e66-9e1c-25dec628bb2d")]
 [assembly: CLSCompliant(true)]
