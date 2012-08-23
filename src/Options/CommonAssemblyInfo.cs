7d0b14a176f3839508350166835e5a0de782be59
David Sidlinger <david.sidlinger@gmail.com>
2012-08-23 08:41:29 -0500

    Bumped version. Added Justin as an author.

--- src/Options/CommonAssemblyInfo.cs
+++ src/Options/CommonAssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("David Sidlinger, Jeff Cutsinger")]
+[assembly: AssemblyCompany("David Sidlinger, Jeff Cutsinger, Justin Pierce")]
 [assembly: AssemblyProduct("Options")]
 [assembly: AssemblyCopyright("Copyright © David Sidlinger 2011")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.31.0.0")]
-[assembly: AssemblyFileVersion("0.31.0.0")]
+[assembly: AssemblyVersion("0.314.0.0")]
+[assembly: AssemblyFileVersion("0.314.0.0")]
